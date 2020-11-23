72a811b8cecf6533fb64594619f6b4fef9c2fa8a
Nahidul Islam <nahidulislamailab@gmail.com>
2020-11-23 18:19:48 +0600

    sum.c Impliment

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+   int a, b, sum;
+
+   printf("\nEnter two no: ");
+   scanf("%d %d", &a, &b,&c);
+
+   sum = a + b+c;
+
+   printf("Sum : %d", sum);
+
+   return(0);
+}
